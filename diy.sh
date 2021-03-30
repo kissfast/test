f4265bd1170c96a23969b14a359419fab47601f6
kissfast <66357967+kissfast@users.noreply.github.com>
2021-03-30 09:44:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,7 @@
 
 ##############################作者昵称（必填）##############################
 # 使用空格隔开
-author_list="i-chenzhe"
+author_list="monk-coder"
 
 ##############################作者脚本地址URL（必填）##############################
 # 例如：https://raw.githubusercontent.com/whyour/hundun/master/quanx/jx_nc.js
